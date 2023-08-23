a8bd277da83563a35823d3d3c670291d58cc42ec
haruyuki <haruyuki.program@gmail.com>
2023-08-23 23:01:55 +0900

    メッセージの登録を実装

--- main.deno.js
+++ main.deno.js
@@ -1,22 +1,22 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import * as dotenv from "https://deno.land/std@0.167.0/dotenv/mod.ts";
-import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
+import * as dotenv from 'https://deno.land/std@0.167.0/dotenv/mod.ts';
+import { serve } from 'https://deno.land/std@0.151.0/http/server.ts';
+import { serveDir } from 'https://deno.land/std@0.151.0/http/file_server.ts';
 
 await dotenv.config({
   export: true,
 
   safe: true,
-  example: ".env.example",
-  path: ".env"
+  example: '.env.example',
+  path: '.env'
 });
 
 const config = Deno.env.toObject();
-console.log(config["SUPABASEURL"], config["SUPABASEKEY"]);
+console.log(config['SUPABASEURL'], config['SUPABASEKEY']);
 
 const supabase = createClient(
-  config["SUPABASEURL"],
-  config["SUPABASEKEY"],
+  config['SUPABASEURL'],
+  config['SUPABASEKEY'],
   {
     auth: {
       persistSession: false
@@ -24,28 +24,50 @@ const supabase = createClient(
   }
 );
 
+const decoder = new TextDecoder();
+
 serve(async (req) => {
   const url = new URL(req.url);
   const pathname = url.pathname;
   console.log(url);
 
-  if (pathname === "/hc" && req.method === "GET") {
-    return new Response("server active");
+  if (pathname === '/hc' && req.method === 'GET') {
+    return new Response('server active');
   }
 
-  if (pathname === "/message") {
-    if (req.method === "GET") {
-      const time = url.searchParams.get("time");
-      const res = await supabase.from("messages").select();
+  if (pathname === '/message') {
+    if (req.method === 'GET') {
+      const time = url.searchParams.get('time');
+      const res = await supabase.from('messages').select();
       return new Response(JSON.stringify(res));
-    } else if (req.method === "POST") {
-      return new Response("now implementing", {status: 500})
+    } else if (req.method === 'POST') {
+      const reader = req.body?.getReader();
+      /**
+       * @type {Uint8Array}
+       */
+      let buf = '';
+      while (true) {
+        const tmp = await reader.read();
+        buf += decoder.decode(tmp?.value);
+        if (tmp.done) break;
+      }
+      const data = JSON.parse(buf);
+      const res = await supabase
+                              .from('messages')
+                              .insert({
+                                message: data.message,
+                                dots: data.dots
+                              })
+                              .select();
+      console.log(res);
+      if (res?.error !== null) return new Response('internal server error', {status: 500});
+      return new Response(JSON.stringify({id: res?.data[0].id}), {status: 201})
     }
   }
 
   return serveDir(req, {
-    fsRoot: "public",
-    urlRoot: "",
+    fsRoot: 'public',
+    urlRoot: '',
     showDirListing: true,
     enableCors: true,
   });
--- public/main.js
+++ public/main.js
@@ -1,14 +1,85 @@
+const isDebug = window.location.host.includes('localhost:800');
+const apiUrlBase = isDebug ? 'http://localhost:8000/' : 'https://chanabi.deno.dev/';
+const xhr = new XMLHttpRequest();
+
 /**
  * @type {HTMLCanvasElement}
  */
 const canvas = document.querySelector('canvas#canvas');
 const ctx = canvas.getContext('2d');
-
-
 ctx.fillStyle = 'rgb(0, 0, 0)';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-ctx.font = `${canvas.height * 0.8}pt "Arial"`;
+/**
+ * @type {HTMLInputElement}
+ */
+const input = document.querySelector('input#message');
+input.addEventListener('change', (ev) => {
+  if (!ev.target.value) return;
+  /**
+   * @type {string}
+   * メッセージ花火で打ち上げる文字列
+   */
+  const message = ev.target.value;
+  const len = message.length;
+  const charSize = (canvas.height) / len;
+  console.log(charSize);
 
-ctx.fillStyle = 'rgb(255, 255, 255)';
-ctx.fillText('hello', 0, canvas.height - canvas.height * 0.1, canvas.width);
+  // 文字の大きさを指定
+  if (len > 0) ctx.font = `${charSize}px "Noto Sans JP"`;
+
+  // canvasの初期化
+  ctx.clearRect(0, 0, canvas.width, canvas.width);
+  ctx.fillStyle = 'rgb(0, 0, 0)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // canvasに文字列を描画
+  ctx.fillStyle = 'rgb(255, 255, 255)';
+  const textSize = ctx.measureText(message);
+  const textHeight = textSize.actualBoundingBoxAscent + textSize.actualBoundingBoxDescent;
+  const textWidth = textSize.width;
+  console.log(textSize);
+  ctx.fillText(message, (canvas.width - textWidth) / 2, (canvas.height + textHeight) / 2);
+});
+
+/**
+ * @type {HTMLButtonElement}
+ */
+const submitButton = document.querySelector('input#submit[type="button"]');
+submitButton.addEventListener('click', () => {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+  console.log(imageData)
+  const dots = [];
+
+  const interval = 3;
+  console.log(canvas.height * canvas.width);
+  for (let i = 0; i < canvas.height; i++) {
+    if (i % interval !== 0) continue;
+    for (let j = 0; j < canvas.width; j++) {
+      if (j % interval !== 0) continue;
+      const base = (i * canvas.width + j) * 4;
+      if (imageData[base] === 255 && imageData[base + 1] === 255 && imageData[base + 2] === 255) {
+        dots.push([j, i]);
+      }
+    }
+  }
+
+  const body = {
+    message: input.value,
+    dots: dots
+  };
+  console.log(body);
+
+  // xhrを使ってAPIリクエストを行う
+  // 参考: https://kinsta.com/jp/knowledgebase/javascript-http-request/
+  xhr.open('post', apiUrlBase + 'message');
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 201) {
+      console.log(JSON.parse(xhr.responseText));
+    } else {
+      console.log(`Error: ${xhr.status}`);
+    }
+  };
+  xhr.send(JSON.stringify(body));
+})
